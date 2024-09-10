4b382a5a947db202807336ab978ece31415867e7
thushar003 <thusharjoji2@gmail.com>
2024-09-10 13:12:37 -0400

    Dark mode is now functional Taking the code from darkmode.js and moving it to app.js finally made the darkmode switch work. darkmode.js is now obsolete and has been deleted

--- app.js
+++ app.js
@@ -8,3 +8,23 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
+
+let darkmode = localStorage.getItem("darkmode")
+const darkmodeSwitch = document.getElementById("darkmode-switch")
+
+const enableDarkmode = () => {
+    document.body.classList.add("darkmode")
+    localStorage.setItem("darkmode", "active")
+}
+
+const disableDarkmode = () => {
+    document.body.classList.remove("darkmode")
+    localStorage.setItem("darkmode", null)
+}
+
+if (darkmode === "active") enableDarkmode()
+
+darkmodeSwitch.addEventListener("click", () => {
+    darkmode = localStorage.getItem("darkmode")
+    darkmode !== "active" ? enableDarkmode() : disableDarkmode()
+})
